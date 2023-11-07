5ad12f099bd6962cc266cc3b37b3d62f2dfee373
unknown <nourhenjbali1@gmail.com>
2023-11-07 17:01:43 +0100

    aading redis

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,19 @@
 'use server'
 
-// import { client } from "@/lib/db"
+ import { client } from "@/lib/db"
 import { redirect } from 'next/navigation'
 
 export async function createBook(formData) {
   const {title, rating, author, blurb} = Object.fromEntries(formData)
+  // create id book
+  const id = Math.floor(Math.random()*100000) ; 
+  // create and save hash in the redis 
 
-  
+  await client.HSet(`books:${id}`,{
+   title,
+   rating,
+   author,
+   blurb,   
+  })
+  redirect("/");
 }
